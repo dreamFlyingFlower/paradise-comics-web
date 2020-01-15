3ecc1cd50fe8c0139961fc293581248d3792e13e
ParadiseWY <582822832@qq.com>
2020-01-15 17:29:10 +0800

    create

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import store from './store'
 import ElementUI from 'element-ui'
 import Api from './global/api'
 import Funcs from './global/funcs'
+import VideoPlayer from 'vue-video-player'
+import 'videojs-contrib-hls'
 // import 'bootstrap'
 import $ from 'jquery'
 
 import 'element-ui/lib/theme-chalk/index.css'
+require('video.js/dist/video-js.css')
+require('vue-video-player/src/custom-theme.css')
+
 
 // 取消vue的所有日志和警告,true取消,false不取消
 Vue.config.silent = false;
@@ -18,12 +23,15 @@ Vue.config.productionTip = false;
 // 避免用控制台操作vue,在正式环境会自动编译为false
 Vue.config.devtools = process.env.NODE_ENV === "development";
 
+// 注入elementui
+Vue.use(ElementUI);
+// 注入video-player
+Vue.use(VideoPlayer);
 // 注入全局通用API
 Vue.use(Api);
 // 注入全局通用函数
 Vue.use(Funcs);
-// 注入elementui
-Vue.use(ElementUI);
+
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/videos/live.vue
+++ src/pages/videos/live.vue
@@ -1,29 +1,22 @@
 <template>
   <div class="player">
-    <!-- 视频播放 -->
     <video-player class="video-player vjs-custom-skin"
-                  ref="videoPlayer"
-                  :playsinline="true"
-                  :options="playerOptions"
-                  @play="playIt($event)"
-                  @pause="pauseIt($event)"></video-player>
-    <!-- 评论 -->
-    <template v-if="$store.state.user.username">
-
-    </template>
-    <template v-else>
-      登录之后才能评论,请<router-link :to="{path:'/login'}">登录</router-link>
-    </template>
+    ref="videoPlayer"
+    :playsinline="true"
+    :options="playerOptions"
+    @play="playIt($event)"
+    @pause="pauseIt($event)"></video-player>
   </div>
 </template>
 
 <script>
+  // 视频直播,未做
   export default {
-    name: "play",
+    name: "live",
     data() {
       return {
         playerOptions: {
-          playbackRates: [0.5, 1.0, 1.5, 2.0], //播放速度
+          playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
           autoplay: true, //如果true,浏览器准备好时开始回放。
           muted: false, // 默认情况下将会消除任何音频。
           loop: false, // 导致视频一结束就重新开始。
@@ -32,11 +25,12 @@
           aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
           fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
           sources: [{
-            type: "video/mp4", //播放类型，如果为rtmp配置为rtmp/mp4
-            // src: "http://localhost:12345/av/av75.mp4" //直播流地址
+            withCredentials: false,
+            type: "application/x-mpegURL", //播放类型，如果为rtmp配置为rtmp/mp4,
+            src: "http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8" //直播流地址
           }],
           poster: "poster.jpg", //你的封面地址
-          // width: document.documentElement.clientWidth/2,
+          width: document.documentElement.clientWidth,
           notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
           controlBar: {
             timeDivider: true,
@@ -45,29 +39,11 @@
             fullscreenToggle: true //全屏按钮
           }
         },
-        comments:[]
       }
     },
     created() {
-      this.playerOptions.sources[0].src = this.$route.params.src;
-      this.getComments();
-    },
-    methods: {
-      playIt() {
-
-      },
-      pauseIt() {
-
-      },
-      // 获得该视频的评论,未登录不显示
-      getComments(){
-        if (!this.$store.state.user.username){
-          // return;
-        }
-        this.$getEntitys('comment').then(resp=>{
-          this.comments = resp.data;
-        })
-      }
+      this.playerOptions.src = this.$route.params.src;
+      console.log(this.$route.params)
     }
   }
 </script>
