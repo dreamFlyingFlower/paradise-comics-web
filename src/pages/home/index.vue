65685c8fad02e8a17871fa3a5f408b616503ccb8
ParadiseWY <582822832@qq.com>
2020-05-25 15:24:55 +0800

    create

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,14 +3,14 @@
     <!-- 番剧推荐 -->
     <el-row :gutter="12" class="index-recommend" style="margin:20px;" v-for="recommend in recommends" :key="recommend.labelKey">
       <h3 v-text="recommend.labelName"></h3>
-      <el-col :span="6" v-for="item in recommend[recommend.labelKey]" :key="item.id">
-        <el-card :body-style="{ padding: '0px' }">
-          <router-link target="_blank" :to="{name:'comic',params:{id:item.id}}">
-            <img :src="item.cover" :alt="item.name" class="image" style="height: 250px;">
+      <el-col :span="6" v-for="item in recommend[recommend.labelKey]" :key="item.id" style="text-align: center;">
+        <el-card :body-style="{ padding: '0px',width: '200px',height: '300px' }">
+          <router-link target="_blank" :to="{ name: 'comic', params: { id: item.id } }">
+            <img :src="item.cover" :alt="item.name" class="image" style="height: 250px;" />
             <div class="name">
               <span class="font-ellipsis" :title="item.name" v-text="item.name"></span>
               <div class="bottom clearfix">
-                <time class="time">{{item.createtime}}</time>
+                <time class="time">{{ item.createtime }}</time>
               </div>
             </div>
           </router-link>
@@ -21,26 +21,26 @@
 </template>
 
 <script>
-  import {getRecommends} from "../../api/comic";
+import { getRecommends } from "../../api/comic";
 
-  export default {
-    name: "index",
-    data() {
-      return {
-        // 番剧推荐
-        recommends: []
-      }
-    },
-    created() {
-      this.getRecommends();
-    },
-    methods: {
-      // 获得番剧推荐
-      getRecommends() {
-        getRecommends().then(resp => {
-          this.recommends = resp.data;
-        });
-      }
+export default {
+  name: "index",
+  data() {
+    return {
+      // 番剧推荐
+      recommends: []
+    };
+  },
+  created() {
+    this.getRecommends();
+  },
+  methods: {
+    // 获得番剧推荐
+    getRecommends() {
+      getRecommends().then(resp => {
+        this.recommends = resp.data;
+      });
     }
   }
+};
 </script>
