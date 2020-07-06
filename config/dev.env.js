326900739ad643afbf9ce3a4911291aac3376616
ParadiseWY <582822832@qq.com>
2020-07-06 09:21:35 +0800

    fix

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT:'"http://localhost:12345/"',
+  API_ROOT:'"http://localhost:12350/"',
   TIME_OUT:'60000'
 });
